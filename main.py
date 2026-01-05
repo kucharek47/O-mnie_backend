9d5351e4d76d060ee807b0b3f069917efa5e8e42
Kucharek47 <matzar0@wp.pl>
2026-01-05 16:07:03 +0100

    0.0.12

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ dane_json = 'dane.json'
 app = Flask(__name__, static_folder='static', template_folder='templates')
 CORS(app)
 
-
 @app.route('/api/dane', methods=['GET'])
 def get_dane():
     if not os.path.exists(dane_json):
@@ -21,6 +20,13 @@ def get_dane():
     except Exception as e:
         return jsonify({"error": f"Błąd serwera: {str(e)}"}), 500
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory('static', path)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
