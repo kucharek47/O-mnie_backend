ab657cce16949a7154743e929142a05912f71315
Kucharek47 <matzar0@wp.pl>
2026-01-05 16:11:19 +0100

    0.1.1

--- main.py
+++ main.py
@@ -29,4 +29,4 @@ def static_files(path):
     return send_from_directory('static', path)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, port=13004)
